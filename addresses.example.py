86334c6e36f0fed6964b7adb459136c701fa3237
miros <miroslavaautumn@gmail.com>
2025-12-02 11:40:45 +0300

    added template for addresses.example.py

---
+++ addresses.example.py
@@ -0,0 +1,8 @@
+# Add your wallet addresses (no more than 5 for a single account)
+ADDRESSES = [
+    "YOUR WALLET ADDRESS",  # 1
+    "YOUR WALLET ADDRESS",  # 2
+    "YOUR WALLET ADDRESS",  # 3
+    "YOUR WALLET ADDRESS",  # 4
+    "YOUR WALLET ADDRESS",  # 5
+]
